1f845d8266fdda8d73b902d1d80077f5341c4607
joanne.snider@maine.edu <joanne.snider@maine.edu>
2023-01-24 18:38:49 -0500

    first commit

---
+++ src/HelloWorldLab2/Lab2Assignment.java
@@ -0,0 +1,7 @@
+package HelloWorldLab2;
+
+public class Lab2Assignment {
+	// This is a new project for lab 2
+	System.out.println("Hello World Lab 2!"); 
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author snide
+ *
+ */
+module HelloWorld_Lab2 {
+}
