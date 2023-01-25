7104cffab46c49977c7aff6458f2f77dd7b2fd00
joanne.snider@maine.edu <joanne.snider@maine.edu>
2023-01-24 19:20:46 -0500

    step 12

--- src/HelloWorldLab2/Lab2Assignment.java
+++ src/HelloWorldLab2/Lab2Assignment.java
@@ -6,8 +6,8 @@ public class Lab2Assignment {
 		// This is a new project for lab 2
 		System.out.println("Hello World Lab 2!"); 
 		
-		// step 3 Hi Alice 
-		System.out.println("Hi, Alice");
+		// step 3 Hi Alice and Bob
+		System.out.println("Hi, Alice and Bob");
 	}
 
 }
